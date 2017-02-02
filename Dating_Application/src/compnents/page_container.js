ac21669cb18243f0442a33efbb876aa65b9ed2ff
CodeandSki <a.k.hove@gmail.com>
2017-02-02 06:10:59 -0700

    basic component structure

---
+++ Dating_Application/src/compnents/page_container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class PageContainer extends Component {
+    render(){
+        return (
+            
+
+
+
+        )
+
+
+    }
+}
