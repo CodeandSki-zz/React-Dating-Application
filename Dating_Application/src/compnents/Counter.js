f8e6365c1a06c8bd4c530fc26b2b93b336325a10
CodeandSki <a.k.hove@gmail.com>
2017-02-05 16:52:34 -0700

    added counter component

---
+++ Dating_Application/src/compnents/Counter.js
@@ -0,0 +1,20 @@
+
+import {Component} from 'react'
+
+class Counter extends Component {
+
+    state = {
+        count: 0
+    };
+
+    render() {
+        return <button onClick={this.increment.bind(this)}>{this.state.count}</button>
+    }
+
+    increment() {
+        this.setState({count: this.state.count + this.props.increment});
+    }
+
+}
+
+export default Counter;
