a42ed72c2ae1f45a94a92ae6910806cc13f7057e
CodeandSki <a.k.hove@gmail.com>
2017-02-09 06:06:14 -0700

    building out input button

---
+++ Dating_Application/src/compnents/input.js
@@ -0,0 +1,15 @@
+class Input extends Component {
+  render() {
+
+      buttonClick() {
+          alert('Proof of concept');
+      }
+
+
+    return (
+      <input className="std-input" onClick={}>Text goes here</input>
+    );
+  }
+}
+
+export default Input;
