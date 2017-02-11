e0b5dbd0726fa2a2c13366f682990a87b6f475d1
CodeandSki <a.k.hove@gmail.com>
2017-02-11 16:29:32 -0700

    added page container to view

--- Dating_Application/src/App.js
+++ Dating_Application/src/App.js
@@ -6,6 +6,7 @@ import logo from './logo.svg';
 import './App.css';
 import Input from './compnents/input';
 import Button from './compnents/button';
+import PageContainer from './compnents/page_container';
 
 
 
@@ -24,21 +25,22 @@ class Slider extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
+     <PageContainer>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>This is the main tagline</h1>
           <h2>bad</h2>
+
         <div className="row">
           <button className="col-sm-3 col-sm-push-3">Test Button</button>
           <button className="col-sm-3 col-sm-push-3">Test Button</button>
-      </div>
+       </div>
         </div>
         <p className="App-intro">
 
             {/* <Counter increment={1}></Counter> */}
         </p>
-      </div>
+    </PageContainer>
     );
   }
 }
