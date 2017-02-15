b6d15345efe824f86acb2f97a2467fedba85479d
CodeandSki <a.k.hove@gmail.com>
2017-02-15 10:02:51 -0700

    Testing page container

--- Dating_Application/src/compnents/page_container.js
+++ Dating_Application/src/compnents/page_container.js
@@ -6,8 +6,8 @@ import React, { Component } from 'react';
 class PageContainer extends Component {
     render(){
         return (
-            <div class="container-fluid">
-                <h1>This is a test of the PageContainer function</h1>
+            <div class="container-fluid page_container">
+
 
 
             </div>
